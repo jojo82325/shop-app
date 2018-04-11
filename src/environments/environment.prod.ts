2e75ad77209b000b171eb276b62d711a9abcff64
Alicia Yu <aliciayu.nz@gmail.com>
2018-04-11 23:17:55 +1200

    update routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASJhwlfVx8_MawcRBAPwooeFj1L81xr6M",
+    authDomain: "shop-app-55985.firebaseapp.com",
+    databaseURL: "https://shop-app-55985.firebaseio.com",
+    projectId: "shop-app-55985",
+    storageBucket: "",
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASJhwlfVx8_MawcRBAPwooeFj1L81xr6M",
+    authDomain: "shop-app-55985.firebaseapp.com",
+    databaseURL: "https://shop-app-55985.firebaseio.com",
+    projectId: "shop-app-55985",
+    storageBucket: "",
+  }
 };
